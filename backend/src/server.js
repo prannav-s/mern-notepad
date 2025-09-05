beb30c451c8cb86dc61ed857fc3ece589e52817a
Prannav Shankar <ps325@duke.edu>
2025-09-05 18:50:39 -0400

    backend

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./rateLimiter.js/rateLimiter.js";
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+
+
+app.use(express.json()); //middleware that will parse JSON bodies: req.body
+app.use(rateLimiter)
+//custom middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next();
+})
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+        app.listen(PORT, () => {
+            console.log("Server started on PORT:", PORT);
+    });
+});
